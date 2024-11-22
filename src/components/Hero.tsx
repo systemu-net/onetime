270526e3c95bb7e00ad8f5c4eef88f3184024161
Sergii Demianchuk <sergeydemjanchyk@gmail.com>
2024-11-21 20:13:36 -0500

    Add hero svg image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
           <div className="hero__content">
             <h1>More than just shorter links</h1>
             <p>
-              Build your brand’s recognition and get detailed insights on how your links are performing.
+              Build your brand's recognition and get detailed insights on how your links are performing.
             </p>
             <a href="#" datatype="narrow" className="btn">Get Started</a>
           </div>
