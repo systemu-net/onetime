17547b4a8c954c68c1eea2cb16ecd2cfacced897
Maryanka Stupen <maryanka.stupen@openpublishing.com>
2024-12-08 21:49:23 -0500

    Added Links page

---
+++ src/pages/Links.tsx
@@ -0,0 +1,10 @@
+import MainLayout from '../components/layouts/MainLayout';
+
+const LinksPage = () => {
+  return (
+    <MainLayout>
+      <h1 className="text-2xl mb-4 text-gray-700">Thinly links</h1>
+    </MainLayout>
+  );
+};
+export default LinksPage;
