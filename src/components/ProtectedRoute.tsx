90d21b3dcbd89109019ff11a2c15c4df86a3734b
Sergii Demianchuk <sergeydemjanchyk@gmail.com>
2024-12-15 01:51:58 -0500

    Add src/index.tsx and update vite.config.ts

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,18 +1,27 @@
 import {useCookies} from 'react-cookie';
 import {LOGIN_ROUTE} from '../routes';
-import {getCurrentUserApi} from '../apis/authentication';
+import {getCurrentUserApi} from "@/apis/authentication";
 
 import React, { useEffect, useState } from 'react';
-import {Navigate, useNavigate} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 
 type ProtectedRouteProps = {
   children: JSX.Element;
   allowedRoles: ('admin' | 'staff')[]; // set the user-roles
 };
 
+interface UserInfo {
+  id: number;
+  email: string;
+  role: 'admin' | 'staff';
+  created_at: string;
+  updated_at: string;
+}
+
+
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({children, allowedRoles}) => {
-  const [cookies, setCookie, removeCookie] = useCookies(['token']);
-  const [user, setUser] = useState<UserInfo>({});
+  const [cookies, , removeCookie] = useCookies(['token']);
+  const [user, setUser] = useState<UserInfo>();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -21,7 +30,6 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({children, allowedRoles})
         const [response, error] = await getCurrentUserApi(cookies.token);
 
         if (error) {
-          debugger;
           console.error(error);
           removeCookie('token');
         } else {
@@ -43,17 +51,11 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({children, allowedRoles})
     }
   }, []);
 
-  if (allowedRoles.includes(user.role)) {
-    return children;
-  } else {
-    return (
-      <>
-        <div className="error-page-container">
-          <h1> Error 404 -- Page Not Found </h1>
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      {user && allowedRoles.includes(user.role) ? children : <h1> Error 404 -- Page Not Found </h1>}
+    </>
+  );
 };
 
 export default ProtectedRoute;
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 4000,
+  },
+  resolve: {
+    alias: {
+      '@': path.join(import.meta.dirname, "src"),
+    },
+  },
+  define: {
+    "process.env": process.env,
   }
 });
 
