eef0ef7d7db3d94a2969f8d55d090bf6471a0c55
Sergii Demianchuk <sergeydemjanchyk@gmail.com>
2025-02-12 13:45:00 -0500

    Update the Profile section

--- src/components/sections/ProfileSection.tsx
+++ src/components/sections/ProfileSection.tsx
@@ -64,7 +64,7 @@ export const ProfileSection = () => {
     <Container className="mt-9">
       <div className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-          Software designer, founder, and amateur astronaut.
+          Senior Software Engineering Technical Leader, founde and CTO.
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
           I'm Sergii CTO and entrepreneur based in North Carolina Cary City.
@@ -79,12 +79,12 @@ export const ProfileSection = () => {
             icon={InstagramIcon}
           />
           <SocialLink
-            to="#"
+            to="https://github.com/ndemianc/"
             aria-label="Follow on GitHub"
             icon={GitHubIcon}
           />
           <SocialLink
-            to="#"
+            to="https://www.linkedin.com/in/sdemian/"
             aria-label="Follow on LinkedIn"
             icon={LinkedInIcon}
           />
