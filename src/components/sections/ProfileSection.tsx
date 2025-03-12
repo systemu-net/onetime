bc947a91222715799daa7d8141b8efb0b24f1aa9
Sergii Demianchuk <sergeydemjanchyk@gmail.com>
2025-03-12 01:38:07 -0400

    fix: profile section

--- src/components/sections/ProfileSection.tsx
+++ src/components/sections/ProfileSection.tsx
@@ -64,7 +64,7 @@ export const ProfileSection = () => {
     <Container className="mt-9">
       <div className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-          Senior Software Engineering Technical Leader, founde and CTO.
+          Your place for exploring the world of then Next generation shortened links.
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
           I'm Sergii CTO and entrepreneur based in North Carolina Cary City.
