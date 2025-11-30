785d6d4dd9a590ad6f11ff6d3c46d51555d708be
Maryanka Stupen <maryanka.stupen@openpublishing.com>
2025-11-30 13:41:45 -0500

    Update user menu

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -8,8 +8,9 @@ import {
   MenuItems,
   TransitionChild,
 } from '@headlessui/react';
-import { ChevronUpIcon } from '@heroicons/react/20/solid';
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/20/solid';
 import {
+  Bars3Icon,
   Cog6ToothIcon,
   DocumentTextIcon,
   HomeIcon,
@@ -265,7 +266,7 @@ const MainLayout = ({ children }) => {
 
         <div className="lg:pl-72">
           {/*FUTURE*/}
-          {/* <div className="border-b border-gray-200 dark:border-zinc-700 sticky top-0 z-40 lg:mx-auto lg:max-w-7xl lg:px-8">
+          <div className="border-b border-gray-200 dark:border-zinc-700 sticky lg:hidden top-0 z-40 lg:mx-auto lg:max-w-7xl lg:px-8">
             <div className="flex h-16 items-center gap-x-4 bg-white dark:bg-zinc-950 shadow-sm sm:gap-x-6 sm:px-6 px-4 lg:shadow-none">
               <button
                 type="button"
@@ -280,7 +281,7 @@ const MainLayout = ({ children }) => {
                 aria-hidden="true"
                 className="h-6 w-px bg-gray-200 lg:hidden"
               />
-              <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
+              {/* <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
                 <form
                   action="#"
                   method="GET"
@@ -313,9 +314,50 @@ const MainLayout = ({ children }) => {
                     className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-200"
                   />
                 </div>
-              </div>
+              </div> */}
+              {/* Profile dropdown */}
+              <Menu as="div" className="relative">
+                <MenuButton className="-m-1.5 flex items-center p-1.5">
+                  <span className="sr-only">Open user menu</span>
+                  <Jdenticon value={cookies.email} size={"32"} className="size-8 rounded-full bg-gray-50" />
+                  <span className="hidden md:flex md:items-center">
+                    <span
+                      aria-hidden="true"
+                      className="ml-4 text-sm/6 font-semibold"
+                    >
+                      {cookies.email}
+                    </span>
+                    <ChevronDownIcon
+                      aria-hidden="true"
+                      className="ml-2 size-5 text-gray-400"
+                    />
+                  </span>
+                </MenuButton>
+                <MenuItems
+                  transition
+                  className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                >
+                  <MenuItem key={`Your profile`}>
+                    <Link
+                      to={PROFILE_ROUTE}
+                      className="block px-3 py-1 text-sm/6 text-gray-900 data-[focus]:bg-gray-50 data-[focus]:outline-none"
+                    >
+                      Your profile
+                    </Link>
+                  </MenuItem>
+                  <MenuItem key={`Sign out`}>
+                    <Link
+                      to={LANDING_ROUTE}
+                      className="block px-3 py-1 text-sm/6 text-gray-900 data-[focus]:bg-gray-50 data-[focus]:outline-none"
+                      onClick={handleLogout}
+                    >
+                      Sign out
+                    </Link>
+                  </MenuItem>
+                </MenuItems>
+              </Menu>
             </div>
-          </div> */}
+          </div>
           <main className="pt-6 flex flex-1 h-full overflow-x-hidden">
             <div className='flex-grow h-full min-w-0'>
               <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
